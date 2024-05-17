504a4f1dea0b9ba6b97adaf2633d638cbdc80c61
BennyIjeoma <iuwabunkeonye@figours.com>
2024-05-17 16:07:41 +0100

    new updates

--- lib/noshNetwork_web/live/auth/user_login_live.ex
+++ lib/noshNetwork_web/live/auth/user_login_live.ex
@@ -3,30 +3,46 @@ defmodule NoshNetworkWeb.Auth.UserLoginLive do
 
   def render(assigns) do
     ~H"""
-    <div class="mx-auto max-w-sm">
-      <.header class="text-center">
+    <div class="mx-auto max-w-sm p-6 font-brand">
+      <div class="mx-auto flex itmes-center justify-center">
+        <a href={~p"/"}>
+          <img src="/images/transparent_logo.png" alt="img" class="w-[150px]" />
+        </a>
+      </div>
+      <.header class="text-center text-black lg:text-3xl text-2xl">
         Sign in to account
         <:subtitle>
           Don't have an account?
-          <.link navigate={~p"/auth/register"} class="font-semibold text-brand hover:underline">
+          <.link
+            navigate={~p"/auth/register"}
+            class="font-semibold text-brand text-white hover:underline"
+          >
             Sign up
           </.link>
           for an account now.
         </:subtitle>
       </.header>
 
-      <.simple_form for={@form} id="login_form" action={~p"/auth/log_in"} phx-update="ignore">
+      <.simple_form
+        for={@form}
+        id="login_form"
+        action={~p"/auth/log_in"}
+        class="bg-white bg-opacity-50 p-6 rounded-md shadow-sm shadow-gray-200 my-4"
+      >
         <.input field={@form[:email]} type="email" label="Email" required />
         <.input field={@form[:password]} type="password" label="Password" required />
 
         <:actions>
           <.input field={@form[:remember_me]} type="checkbox" label="Keep me logged in" />
+        </:actions>
+
+        <:actions>
           <.link href={~p"/auth/reset_password"} class="text-sm font-semibold">
             Forgot your password?
           </.link>
         </:actions>
         <:actions>
-          <.button phx-disable-with="Signing in..." class="w-full">
+          <.button phx-disable-with="Signing in..." class="w-full my-3">
             Sign in <span aria-hidden="true">→</span>
           </.button>
         </:actions>
