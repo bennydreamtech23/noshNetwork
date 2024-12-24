68ea023836707b3a5a4ef2775aa37061ae2cc752
BennyIjeoma <iuwabunkeonye@figours.com>
2024-12-24 19:37:07 +0100

    new updates on cater dashboard

---
+++ lib/noshNetwork_web/live/dashboard_live/components/cater_dashboard.ex
@@ -0,0 +1,11 @@
+defmodule NoshNetworkWeb.DashboardLive.Components.CaterDashboard do
+  use NoshNetworkWeb, :live_component
+
+  def render(assigns) do
+    ~H"""
+    <div>
+      hello
+    </div>
+    """
+  end
+end
